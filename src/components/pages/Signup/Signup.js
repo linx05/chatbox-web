a6cfa7b2db6d58163f3127da3bcf53a51792248c
Jorge Chavez <jorge.chavez@points.com>
2019-06-22 13:45:29 -0700

    Add sign up view

--- src/components/pages/Signup/Signup.js
+++ src/components/pages/Signup/Signup.js
@@ -1,63 +1,129 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 import API from '../../../services/API';
 
 export class Signup extends Component {
-  state = {};
+  state = {
+    user: {}
+  };
 
-  submitUser = (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
 
     API.makeRequest({
-      data: this.state,
+      data: this.state.user,
       method: 'POST',
       url: 'users',
     })
     .then(data => {
-      console.log(data);
       alert('Created user!');
       this.setState({
         redirectToLogin: true,
       });
     })
     .catch(err => {
-      console.log(err);
       alert('Could not create user');
     })
   }
 
-  onChangeName = (e) => this.setState({ name: e.target.value });
-
-  onChangeUsername = (e) => this.setState({ username: e.target.value });
-
-  onChangeEmail = (e) => this.setState({ email: e.target.value });
-
-  onChangePassword = (e) => this.setState({ password: e.target.value });
+  onChange = (e) => {
+    let { user } = this.state;
+    user[e.target.name] = e.target.value;
+    this.setState({user});
+  }
 
   render() {
-    if (this.state.redirectToLogin) {
+    const { name, username, email, password, redirectToLogin } = this.state;
+    if (redirectToLogin) {
       return <Redirect to="/login" />
     }
-
-    return (<div>
+    return (
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
       <div>
-        Name: <input type="text" onChange={this.onChangeName} value={this.state.name} />
+        <Grid>
+          <Avatar>
+            <LockOutlinedIcon />
+          </Avatar>
+        </Grid>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form noValidate onSubmit={this.handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            onChange={this.onChange}
+            value={name}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            onChange={this.onChange}
+            value={name}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            onChange={this.onChange}
+            value={email}
+            autoComplete="email"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            onChange={this.onChange}
+            value={password}
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign Up
+          </Button>
+        </form>
       </div>
-      <div>
-        Username:
-        <input type="text" onChange={this.onChangeUsername} value={this.state.username} />
-      </div>
-      <div>
-        Email:
-        <input type="email" onChange={this.onChangeEmail} value={this.state.email} />
-      </div>
-      <div>
-        Password:
-        <input type="password" onChange={this.onChangePassword} value={this.state.password} />
-      </div>
-      <div>
-        <button onClick={this.submitUser}>Create Account</button>
-      </div>
-    </div>);
+    </Container>
+    )
   }
 }
