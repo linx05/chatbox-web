350ba2905fc9385f4926e83d5172027ab9bc89b2
Jorge Chavez <jorge.chavez@points.com>
2019-06-22 13:55:21 -0700

    Remove useless libraries

--- src/components/pages/Signup/Signup.js
+++ src/components/pages/Signup/Signup.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {
+  Container,
+  Typography,
+  Button,
+  CssBaseline,
+  TextField
+} from '@material-ui/core';
 
 import API from '../../../services/API';
 
 export class Signup extends Component {
+
   state = {
     user: {}
   };
@@ -53,77 +48,72 @@ export class Signup extends Component {
     }
     return (
       <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div>
-        <Grid>
-          <Avatar>
-            <LockOutlinedIcon />
-          </Avatar>
-        </Grid>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form noValidate onSubmit={this.handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            autoComplete="name"
-            autoFocus
-            onChange={this.onChange}
-            value={name}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            onChange={this.onChange}
-            value={name}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            onChange={this.onChange}
-            value={email}
-            autoComplete="email"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            onChange={this.onChange}
-            value={password}
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-          >
-            Sign Up
-          </Button>
-        </form>
-      </div>
-    </Container>
+        <CssBaseline />
+        <div>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              onChange={this.onChange}
+              value={name}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              onChange={this.onChange}
+              value={name}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              onChange={this.onChange}
+              value={email}
+              autoComplete="email"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={this.onChange}
+              value={password}
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </Container>
     )
   }
 }
